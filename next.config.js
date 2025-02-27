120a697650d04a638c008689f1002765af6fe758
Hasan Mehdi Rizvi <hasanmrizvi72@gmail.com>
2025-02-28 02:05:18 +0530

    Fixing GitHub Pages deployment
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/rizvihasan/quadtree-visualizer?shareId=XXXX-XXXX-XXXX-XXXX).

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /* eslint-disable no-undef */
 const debug = process.env.NODE_ENV !== 'production'
+const prod = process.env.NODE_ENV === 'production'
+
 module.exports = {
   exportPathMap: function () {
     return {
       '/': { page: '/' },
     }
   },
-  assetPrefix: !debug ? '' : '',
+  assetPrefix: prod ? '/quadtree-visualizer/' : '',
+  basePath: prod ? '/quadtree-visualizer' : '',
 }
